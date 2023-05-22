5db8609a78a67f000782c4d472e5df27f188a149
WarrenT <warrentyler1024@gmail.com>
2023-05-22 17:15:45 +0100

    store image at cloudinary and use url in database post

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,8 +4,47 @@ import { v2 as cloudinary } from "cloudinary";
 
 import Post from "../mongodb/models/post.js";
 
-dotenv.config();
+// dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+router.get("/", async (req, res) => {
+  // Get all the posts from the database.
+  try {
+    const posts = await Post.find({});
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  // Create a post that stores the url of the photo in the database,
+  // after first uploading the photo to cloudinary.
+  // This avoids storing the actual photo in the database to save space.
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    // Store the new post in the database.
+    const newPost = new Post({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 
 export default router;
