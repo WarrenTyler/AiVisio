3467793540ed8bc0f1045567249342782381e795
WarrenT <warrentyler1024@gmail.com>
2023-05-31 04:28:29 +0100

    fix: Add missing Tailwind CSS classes to IconButton
    Fix the IconButton component by including the missing 'object-contain'
    and 'invert' Tailwind CSS classes in the className prop of the image element.
    This ensures proper containment and color inversion of the image icon within
    the IconButton. The component now displays the desired styles as intended.

--- client/src/components/IconButton.jsx
+++ client/src/components/IconButton.jsx
@@ -5,7 +5,7 @@ const IconButton = ({ onClick, icon, text }) => {
       onClick={onClick}
       className="flex items-center space-x-1 text-gray-600 hover:text-gray-900 focus:outline-none"
     >
-      <img src={icon} alt={text} className="w-6 h-6" />
+      <img src={icon} alt={text} className="w-6 h-6 object-contain invert" />
       <span>{text}</span>
     </button>
   );
