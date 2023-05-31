a2a1c9d6e01a2ef0b393d893d33c4830ff4d33e9
WarrenT <warrentyler1024@gmail.com>
2023-05-31 04:05:31 +0100

    fix: Update export in index.js
    Update export in index.js to include newly added IconButton component.
    Ensures other modules can import and use IconButton component correctly.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ import Card from "./Card";
 import FormField from "./FormField";
 import UserInitial from "./UserInitial";
 import UserInfo from "./UserInfo";
+import IconButton from "./IconButton";
 import Loader from "./Loader";
 
-export { Card, FormField, UserInitial, UserInfo, Loader };
+export { Card, FormField, UserInitial, UserInfo, IconButton, Loader };
